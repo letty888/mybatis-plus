66488f34286de71b7be9732121f5de90740f7335
张欢 <1789241291@qq.com>
2020-04-17 14:58:16 +0800

    首次提交

---
+++ itcast-mp-springboot/src/main/java/cn/itcast/mp/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package cn.itcast.mp.mapper;
+
+import cn.itcast.mp.pojo.User;
+
+public interface UserMapper extends MyBaseMapper<User> {
+
+    User findById(Long id);
+
+    User findByUsername(String username);
+
+
+
+}
